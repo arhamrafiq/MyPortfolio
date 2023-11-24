15da54adc101a42271b6d5c1d130bee772e47864
arhamrafiq <148708074+arhamrafiq@users.noreply.github.com>
2023-11-24 11:46:27 +0500

    Update index.js
    new update in main version

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__path, "./my-react-app/build")));
+app.use(express.static(path.join(__dirname, "./my-react-app/build")));
 
 // Routes
 app.use("/api/v1/reg", authRoute);
